a90a81ad3dff053201020768707176214b824a68
dani-MacOS <sio2guanoeleo@gmail.com>
2022-05-18 19:14:45 +0200

    python intro

--- Week1 - Python Introduction/ex05/state.py
+++ Week1 - Python Introduction/ex05/state.py
@@ -15,7 +15,9 @@ capital_cities = {
 
 if (len(sys.argv) != 2):
 	exit("Argoument error")
-try:
-	print(capital_cities[states[sys.argv[1].capitalize()]])
-except:
-	print("Unknown state")
+list = [k for k, v in capital_cities.items() if v == sys.argv[1].capitalize()]
+
+if (len(list)):
+	print([k for k, v in states.items() if v == list[0]][0])
+else:
+	print("Unknown capital city")
